0d2af9b792766230767ffcc3628a28388e96960e
李昊航 <lihaohang@Mine-MacBook.hitronhub.home>
2019-08-11 23:26:21 -0700

    utps-1: initial swagger as router

--- route/routes.js
+++ route/routes.js
@@ -1,20 +1,21 @@
 var express     = require('express');
 var router      = express.Router();
-const newPost   = require('./api/helper/model/poster');
+const newPost   = require('../api/helper/model/poster');
 
 router.get('/abc',(req, res, next) =>{
     res.send('test success');
 })
 router.post('/addpost',(req, res, next) =>{
-    res.send('add  success');
+    console.log('add  success',res);
+    // res.send('add  success',req);
     let newposterItem = new newPost({
         location: req.body.location,
-        picture: req.body.picture,
+        pictures: req.body.pictures,
         content: req.body.content
     });
     newposterItem.save((err, item) =>{
         if(err){
-            res.json(err)
+            console.log(err);
         }
         else{
             res.json({msg: 'Item has been added'});
